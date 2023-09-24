c135dd557aa9740405f4a7d97d4df6c070b97167
aryanboloori97 <arianbolori@gmail.com>
2023-09-24 20:58:50 +0330

    /bin removed from staging git ignore added - bin/ added to it 3 methods added to Demo class: 1- addElementsToBeginning 2- addElementsToEnd 3- addElementsToMiddle

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,9 +1,68 @@
 package demo;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Demo {
 public static void main(String[] args) {
+
 	
 	
-	System.out.println("hello");
+	List<Integer> arrayList1 =  new ArrayList<Integer>();
+	List<Integer> linkedList1 = new LinkedList<Integer>();
+	
+	for (int i=0;i<100;i++) {
+		
+		arrayList1.add(i);
+		linkedList1.add(i);
+		
+	}		
+	
+	System.out.println(arrayList1);
+	addElementsToMiddle(arrayList1, 50);
+	}
+	
+public static void addElementsToBeginning(List<Integer> list, int numberOfElements) {
+
+	
+		
+		for (int i=0; i<numberOfElements; i++) {
+			list.add(i, i);
+		}
+
+
+
 }
+public static void addElementsToMiddle(List<Integer> list, int numberOfElements) {
+
+	
+	
+	int indexToAdd = (int)(list.size()/2);
+	for (int i=0;i<numberOfElements; i++) {
+		System.out.println(indexToAdd);
+		list.add(indexToAdd, i);
+		indexToAdd++;
+	}
+
 }
+
+public static void addElementsToEnd(List<Integer> list, int numberOfElements) {
+
+	
+	
+	for (int i=0; i<numberOfElements; i++) {
+		
+		
+		list.add(i);
+	}
+System.out.println(list);
+
+
+}
+	
+	
+}
+	
+	
+
+
